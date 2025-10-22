3da0ed2a2df3da71e3c8d01fa8eac0335e7b8921
nayeli <nayeliserritos@gmail.com>
2025-10-21 23:37:23 -0600

    Se usó la clase StateRestorationTest.kt para simular el cambio y comprobar la restauración del contenido. En pantallas compactas, se validó que el correo seleccionado siga visible después del cambio de orientación. En pantallas extendidas, se comprobó que el correo abierto en la vista de detalles permanezca mostrado tras la restauración.

--- app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
+++ app/src/androidTest/java/com/example/reply/test/ReplyAppStateRestorationTest.kt
@@ -83,8 +83,12 @@ class ReplyAppStateRestorationTest {
 
         // Verify that third email is displayed on the details screen
         composeTestRule.onNodeWithTagForStringId(R.string.details_screen).onChildren()
-            .assertAny(hasAnyDescendant(hasText(
-                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)))
+            .assertAny(
+                hasAnyDescendant(
+                    hasText(
+                        composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+                    )
+                )
             )
 
         // Simulate a config change
@@ -92,8 +96,12 @@ class ReplyAppStateRestorationTest {
 
         // Verify that third email is still displayed on the details screen
         composeTestRule.onNodeWithTagForStringId(R.string.details_screen).onChildren()
-            .assertAny(hasAnyDescendant(hasText(
-                composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)))
+            .assertAny(
+                hasAnyDescendant(
+                    hasText(
+                        composeTestRule.activity.getString(LocalEmailsDataProvider.allEmails[2].body)
+                    )
+                )
             )
     }
 }
